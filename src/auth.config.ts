c7244b6c5510ad4e94bed1497a0ce7e16d8f0bb4
royerm <royerm@gmail.com>
2024-12-02 16:08:45 +0100

    Comment unused variable

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -2,13 +2,14 @@ import { isPlatform } from "@ionic/react";
 
 export const domain = "royerm.eu.auth0.com";
 export const clientId = "bVa2PDrmp8zmJ9tumzIrDa1lmgQ9IkxX";
-const appId = "io.ionic.starter";
+
 
 // Use `auth0Domain` in string interpolation below so that it doesn't
 // get replaced by the quickstart auto-packager
-const auth0Domain = domain;
 
-const iosOrAndroid = isPlatform('ios') || isPlatform('android');
+// const appId = "io.ionic.starter";
+// const auth0Domain = domain;
+// const iosOrAndroid = isPlatform('ios') || isPlatform('android');
 
 // export const callbackUri = iosOrAndroid
 //  ? `${appId}://${auth0Domain}/capacitor/${appId}/callback`
