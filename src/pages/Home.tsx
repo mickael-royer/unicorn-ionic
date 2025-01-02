ef2185343a14469d0978e459844822a780d11b2c
royerm <royerm@gmail.com>
2025-01-02 23:49:19 +0100

    Update Keys

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,7 +19,8 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRoCk2y6NsEkni0oXb3EAMk1kiL_Y8sd4",
+  key: "AIzaSyCRoCk2y6NsEkni0oXb3EAMk1kiL_Y8sd4",
+  apiKey: "AIzaSyAca7ZQQqwIISsGatmJ-95fz4fyn-YOFMM",
   authDomain: "unicorn-firebase-423a9.firebaseapp.com",
   projectId: "unicorn-firebase-423a9",
   storageBucket: "unicorn-firebase-423a9.firebasestorage.app",
